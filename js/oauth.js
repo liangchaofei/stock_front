9f5c3fa18592de9c737e470ee17d56b007efafbe
liangchaofei <liangchaofei@bytedance.com>
2020-01-19 18:20:14 +0800

    add

--- js/oauth.js
+++ js/oauth.js
@@ -1,22 +1,30 @@
 /*
  * @Author: your name
- * @Date: 2020-01-03 15:33:09
- * @LastEditTime : 2020-01-07 17:19:02
- * @LastEditors  : Please set LastEditors
+ * @Date: 2020-01-19 14:09:47
+ * @LastEditTime: 2020-01-19 18:20:02
+ * @LastEditors: your name
  * @Description: In User Settings Edit
- * @FilePath: /gp/static/js/oauth.js
+ * @FilePath: /gp2/static/js/oauth.js
  */
-$.ajax({
-    url: 'https://stock.zhixiutec.com/api/user',
-    type: 'get',
-    success: function (res) {
-        var data = res.data;
-        if (res.error_code === 2) {
-            window.open(data, "_self");
-        }else{
-            $('#box').css({"display":"block"})
-        }
-    }
-})
+// /*
+//  * @Author: your name
+//  * @Date: 2020-01-03 15:33:09
+//  * @LastEditTime : 2020-01-07 17:19:02
+//  * @LastEditors  : Please set LastEditors
+//  * @Description: In User Settings Edit
+//  * @FilePath: /gp/static/js/oauth.js
+//  */
+// $.ajax({
+//     url: 'https://stock.zhixiutec.com/api/user',
+//     type: 'get',
+//     success: function (res) {
+//         var data = res.data;
+//         if (res.error_code === 2) {
+//             window.open(data, "_self");
+//         }else{
+//             $('#box').css({"display":"block"})
+//         }
+//     }
+// })
 
 
